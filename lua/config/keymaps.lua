1b9a26d62fc39758fc87d09a38a87f5a3aed959c
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-11-17 23:18:54 -0500

    feat: add some keymaps and lsp border and fix tab bug

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = require("lazyvim.util").safe_keymap_set
+
+map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+map("n", "<C-C>", "<cmd> %y+ <cr>`]", { desc = "Copy entire file" })
+map("n", "<C-A>", "ggVG`]", { desc = "Select entire file" })
+
+vim.keymap.set("n", "<C-/>", function()
+  require("lazyvim.util").terminal(nil, { border = "rounded" })
+end, { desc = "Term with border" })
