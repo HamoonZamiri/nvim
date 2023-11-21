d14f3ddf74df4eafea059b4a1994df9917e1473e
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-11-21 13:33:58 -0500

    feat(keymaps): add K and J remaps to move screen up and down

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,8 +11,14 @@ map("n", "<leader>cl", "<nop>") -- LspInfo
 map("n", "<leader>L", "<nop>") -- LazyVim changelog
 
 -- add
+
 map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- move screen up/down
+map("n", "J", "<C-e>", { desc = "Move screen down" })
+map("n", "K", "<C-y>", { desc = "Move screen up" })
+map("n", "<leader>K", "<cmd> lua vim.lsp.buf.hover() <cr>", { desc = "Hover" }) -- Change previous K to <leader>K
 
 map("n", "<C-C>", "<cmd> %y+ <cr>", { desc = "Copy entire file" })
 map("n", "<C-A>", "ggVG", { desc = "Select entire file" })
