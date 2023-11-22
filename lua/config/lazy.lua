e0a6e67bab676a4d2e9dfa268cd79714c6b070b0
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-11-22 02:05:24 -0500

    feat(latex): add latex support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+
+    { import = "lazyvim.plugins.extras.lang.tex" },
     -- import/override with your plugins
     { import = "plugins" },
   },
