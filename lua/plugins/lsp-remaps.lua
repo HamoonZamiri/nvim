da6ef311e96840a02b90c63a5fbebe62311e580a
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-12-02 23:33:21 -0500

    feat(keymaps): change keys J+K for scroll to Ctrl j+k and add <S-k>

--- lua/plugins/lsp-remaps.lua
+++ lua/plugins/lsp-remaps.lua
@@ -1,15 +1,17 @@
--- https://github.com/LazyVim/LazyVim/discussions/758
+-- DISABLE K FOR HOVER
+
+-- -- https://github.com/LazyVim/LazyVim/discussions/758
 return {
-  {
-    "neovim/nvim-lspconfig",
-    init = function()
-      local lsp_keys = require("lazyvim.plugins.lsp.keymaps")
-      local keys = lsp_keys.get()
-      for _, key in ipairs(keys) do
-        if key[1] == "K" then
-          key[1] = "<nop>"
-        end
-      end
-    end,
-  },
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     init = function()
+  --       local lsp_keys = require("lazyvim.plugins.lsp.keymaps")
+  --       local keys = lsp_keys.get()
+  --       for _, key in ipairs(keys) do
+  --         if key[1] == "K" then
+  --           key[1] = "<nop>"
+  --         end
+  --       end
+  --     end,
+  --   },
 }
