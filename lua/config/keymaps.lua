3f212deba75b425c00f0619f369ecba32767f1cc
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-12-17 21:31:50 -0500

    feat(harpoon): update harpoon to v2

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,16 +37,19 @@ end, { desc = "LSP workspace symbols" })
 --[[ Harpoon mappings ]]
 -- stylua: ignore
 if vim.bo["filetype"] ~= "rnvimr" then
-  map("n", "<leader>1", function() require("harpoon.ui").nav_file(1) end, { desc = "Harpoon 1" })
-  map("n", "<leader>2", function() require("harpoon.ui").nav_file(2) end, { desc = "Harpoon 2" })
-  map("n", "<leader>3", function() require("harpoon.ui").nav_file(3) end, { desc = "Harpoon 3" })
-  map("n", "<leader>4", function() require("harpoon.ui").nav_file(4) end, { desc = "Harpoon 4" })
-  map("n", "<leader>5", function() require("harpoon.ui").nav_file(5) end, { desc = "Harpoon 5" })
-  map("n", "<leader>6", function() require("harpoon.ui").nav_file(6) end, { desc = "Harpoon 6" })
-  map("n", "<leader>7", function() require("harpoon.ui").nav_file(7) end, { desc = "Harpoon 7" })
-  map("n", "<leader>8", function() require("harpoon.ui").nav_file(8) end, { desc = "Harpoon 8" })
-  map("n", "<leader>9", function() require("harpoon.ui").nav_file(9) end, { desc = "Harpoon 9" })
-  map("n", "<leader>0", function() require("harpoon.ui").nav_file(0) end, { desc = "Harpoon 0" })
+  local harpoon = require("harpoon")
+  map("n", "<leader>a", function() harpoon:list():append() end, { desc = "Harpoon add mark" })
+  map("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Toggle" })
+  map("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
+  map("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
+  map("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon 3" })
+  map("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon 4" })
+  map("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon 5" })
+  map("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "Harpoon 6" })
+  map("n", "<leader>7", function() harpoon:list():select(7) end, { desc = "Harpoon 7" })
+  map("n", "<leader>8", function() harpoon:list():select(8) end, { desc = "Harpoon 8" })
+  map("n", "<leader>9", function() harpoon:list():select(9) end, { desc = "Harpoon 9" })
+  map("n", "<leader>0", function() harpoon:list():select(0) end, { desc = "Harpoon 0" })
 end
 
 -- [[ git diff mappings ]]
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,24 +1,10 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = "nvim-lua/plenary.nvim",
-    keys = {
-      {
-        "<leader>a",
-        function()
-          require("harpoon.mark").add_file()
-        end,
-        silent = true,
-        desc = "Add harpoon mark",
-      },
-      {
-        "<leader>h",
-        function()
-          require("harpoon.ui").toggle_quick_menu()
-        end,
-        silent = true,
-        desc = "Harpoon toggle",
-      },
-    },
+    setup = function()
+      require("harpoon"):setup()
+    end,
   },
 }
