aa0d1883c76b31eeb3757588b69f385e63ac829d
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-01-19 17:22:54 -0500

    feat(neo-tree): add neo-tree "O" mapping to open file with default app

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,5 +6,17 @@ return {
       group_empty_dirs = true,
       scan_mode = "deep",
     },
+    window = {
+      mappings = {
+        ["O"] = {
+          command = function(state)
+            local filepath = state.tree:get_node().path
+
+            os.execute("open " .. filepath)
+          end,
+          desc = "open_with_system_defaults",
+        },
+      },
+    },
   },
 }
