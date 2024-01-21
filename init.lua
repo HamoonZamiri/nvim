fc7ca1d77aed2cdd6e16f13019469030ddab2a3e
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-01-21 18:14:17 -0500

    feat(latex): add VimTex option to move aux files

--- init.lua
+++ init.lua
@@ -1,6 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- VimTex configs
-vim.g.vimtex_view_method = "skim"
-vim.g.vimtex_compiler_method = "latexmk"
---
+++ lua/plugins/latex.lua
@@ -0,0 +1,10 @@
+return {
+  "lervag/vimtex",
+  init = function()
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = ".auxfiles",
+    }
+  end,
+}
