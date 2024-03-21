5405171ca0096030ed8684d9978e0065e641cc37
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-03-21 00:37:31 -0400

    feat(typescript): use typescript-tools as LSP

--- lua/plugins/languages/typescript.lua
+++ lua/plugins/languages/typescript.lua
@@ -1,5 +1,37 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+      end
+    end,
+  },
+
+  -- LSP
+  {
+    "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "typescriptreact" },
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+
+  -- Disable tsserver with lspconfig since we're using typescript-tools.nvim
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      handlers = {
+        function(server_name)
+          if server_name == "tsserver" then
+            return
+          end
+        end,
+      },
+    },
+  },
+
+  -- Utils
+  {
     "dmmulroy/tsc.nvim",
     opts = {},
     cmd = "TSC",
@@ -7,6 +39,60 @@ return {
   {
     "dmmulroy/ts-error-translator.nvim",
     opts = {},
-    ft = "typescript",
+    ft = { "typescript", "typescriptreact" },
+  },
+
+  -- DAP (Debugger)
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          table.insert(opts.ensure_installed, "js-debug-adapter")
+        end,
+      },
+    },
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["pwa-node"] then
+        require("dap").adapters["pwa-node"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "node",
+            -- 💀 Make sure to update this path to point to your installation
+            args = {
+              require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+                .. "/js-debug/src/dapDebugServer.js",
+              "${port}",
+            },
+          },
+        }
+      end
+      for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+        if not dap.configurations[language] then
+          dap.configurations[language] = {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
+            },
+          }
+        end
+      end
+    end,
   },
 }
--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -5,6 +5,13 @@ return {
       presets = {
         lsp_doc_border = true,
       },
+      lsp = {
+        hover = {
+          -- If hover is not available don't show message
+          -- Useful in TypeScript
+          silent = true,
+        },
+      },
     },
   },
 }
