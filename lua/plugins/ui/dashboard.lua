fa12d7089ebcf45cbf4d39a39f8946084e018b24
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-04-02 01:51:41 -0400

    feat(dashboard): add "g" to open lazygit
    Also removed useless items like "new file" and "lazy extras"

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -14,10 +14,18 @@ local logo = {
 
 return {
   "nvimdev/dashboard-nvim",
-  opts = {
-    config = {
-      -- set the above logo and format it to be centered on the screen
-      header = vim.split(string.rep("\n", 8) .. table.concat(logo, "\n") .. "\n\n", "\n"),
-    },
-  },
+  opts = function(_, opts)
+    -- set the above logo and format it to be centered on the screen
+    opts.config.header = vim.split(string.rep("\n", 8) .. table.concat(logo, "\n") .. "\n\n", "\n")
+    table.remove(opts.config.center, 2) -- Remove "new file"
+    table.remove(opts.config.center, 6) -- Remove "lazy extras"
+
+    -- Add lazygit as an option with "g"
+    local lazygit_item = opts.config.center[3]
+    lazygit_item.action = "lua LazyVim.lazygit()"
+    lazygit_item.desc = "  LazyGit"
+    lazygit_item.icon = ""
+
+    return opts
+  end,
 }
