aa8320aec6204077ee27ef5cb2066ca06861465c
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-07-24 00:21:24 -0400

    feat(keybindings): add option backspace in warp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,31 +12,37 @@ vim.diagnostic.config({
 vim.opt.mousescroll = "ver:1,hor:6" -- Default: "ver:3,hor:6"
 
 vim.g.root_spec = { "cwd", { ".git", "lua" }, "lsp" }
+-- Add the following options to make tailwind classes more visually appealing
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.keymap.set("i", "<m-bs>", "<c-w>", { noremap = true, silent = true })
+vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
 
 vim.opt.clipboard = "unnamedplus"
 
 -- Copy from neovim https://github.com/neovim/neovim/blob/359763307546ae7a401e9458fefeb6f4cd37a40f/runtime/lua/vim/shared.lua#L982
 local function validate(name, value, validator, optional, message)
-    local err_msg --- @type string?
-    if validator then -- Form 1
-      -- Check validator as a string first to optimize the common case.
-      local ok = (type(value) == validator) or (value == nil and optional == true)
-      if not ok then
-        local msg = type(optional) == 'string' and optional or message --[[@as string?]]
-        -- Check more complicated validators
-        err_msg = is_valid(name, value, validator, msg, false)
-      end
-    elseif type(name) == 'table' then -- Form 2
-      vim.deprecate('vim.validate', 'vim.validate(name, value, validator, optional_or_msg)', '1.0')
-      err_msg = validate_spec(name)
-    else
-      error('invalid arguments')
+  local err_msg --- @type string?
+  if validator then -- Form 1
+    -- Check validator as a string first to optimize the common case.
+    local ok = (type(value) == validator) or (value == nil and optional == true)
+    if not ok then
+      local msg = type(optional) == "string" and optional or message --[[@as string?]]
+      -- Check more complicated validators
+      err_msg = is_valid(name, value, validator, msg, false)
     end
-
-    if err_msg then
-      error(err_msg, 2)
-    end
+  elseif type(name) == "table" then -- Form 2
+    vim.deprecate("vim.validate", "vim.validate(name, value, validator, optional_or_msg)", "1.0")
+    err_msg = validate_spec(name)
+  else
+    error("invalid arguments")
   end
+
+  if err_msg then
+    error(err_msg, 2)
+  end
+end
 
 -- Copy vim.ui.open from latest neovim https://github.com/neovim/neovim/blob/359763307546ae7a401e9458fefeb6f4cd37a40f/runtime/lua/vim/ui.lua#L136
 vim.ui.open = function(path, opt)
