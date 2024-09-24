70a9d1848de5b0d06d67805befb81278c0f6e086
Uthman Mohamed <uthmanmoh@icloud.com>
2024-09-24 11:09:40 -0400

    fix(python): remove unnecessary virtualenv autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,18 +23,6 @@ autocmds("FileType", {
   desc = "Toggle word wrap for Latex and Markdown files",
 })
 
-autocmds("VimEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*",
-  callback = function()
-    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-    if venv ~= "" then
-      require("venv-selector").retrieve_from_cache()
-    end
-  end,
-  once = true,
-})
-
 autocmds("FileType", {
   desc = "Set indent to 4 spaces for kt files",
   pattern = { "kt" },
