fc12b936d5d649f9b875abfe1e1eba99e72be154
Uthman Mohamed <uthmanmo@amazon.com>
2024-11-08 15:56:16 +0000

    feat(bufferline): change tabs style to slant

---
+++ lua/plugins/ui/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        separator_style = "slant",
+        -- indicator = {
+        --   style = 'underline',
+        -- },
+        hover = {
+          enabled = true,
+        },
+      }
+    }
+  },
+}
