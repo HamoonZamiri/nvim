557e3d3ad29dd3f6de16f6bc599713aa3d71c4af
Uthman Mohamed <uthmanmo@amazon.com>
2024-11-20 20:53:01 +0000

    fix(dashboard): update to use snacks dashboard

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -1,31 +1,34 @@
--- Taken from https://github.com/MaximilianLloyd/ascii.nvim/blob/master/lua/ascii/text/neovim.lua
-local logo = {
-  [[                                                                       ]],
-  [[                                                                     ]],
-  [[       ████ ██████           █████      ██                     ]],
-  [[      ███████████             █████                             ]],
-  [[      █████████ ███████████████████ ███   ███████████   ]],
-  [[     █████████  ███    █████████████ █████ ██████████████   ]],
-  [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-  [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-  [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-  [[                                                                       ]],
-}
 
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    -- set the above logo and format it to be centered on the screen
-    opts.config.header = vim.split(string.rep("\n", 8) .. table.concat(logo, "\n") .. "\n\n", "\n")
-    table.remove(opts.config.center, 2) -- Remove "new file"
-    table.remove(opts.config.center, 6) -- Remove "lazy extras"
-
-    -- Add lazygit as an option with "g"
-    local lazygit_item = opts.config.center[3]
-    lazygit_item.action = "lua LazyVim.lazygit()"
-    lazygit_item.desc = "  LazyGit"
-    lazygit_item.icon = ""
-
-    return opts
-  end,
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          -- Taken from https://github.com/MaximilianLloyd/ascii.nvim/blob/master/lua/ascii/text/neovim.lua
+          header = [[                                                                       
+                                                                               
+                 ████ ██████           █████      ██                     
+                ███████████             █████                             
+                █████████ ███████████████████ ███   ███████████   
+               █████████  ███    █████████████ █████ ██████████████   
+              █████████ ██████████ █████████ █████ █████ ████ █████   
+            ███████████ ███    ███ █████████ █████ █████ ████ █████  
+           ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+                                                                                 ]],
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find Files", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "g", desc = "LazyGit", action = ":lua Snacks.lazygit()" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+      },
+    },
+  },
 }
