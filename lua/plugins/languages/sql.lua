ac949222631c5386e09b2c23adc4a8ffa66a5460
Hamoon Zamiri <hamoonzamiri@Mac-mini.local>
2026-04-05 20:31:55 -0400

    feat(sql): enable sqlfluff formatter

---
+++ lua/plugins/languages/sql.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sqlfluff" },
+      },
+      formatters = {
+        sqlfluff = {
+          args = { "format", "--dialect=postgres", "-" },
+        },
+      },
+    },
+  },
+}
